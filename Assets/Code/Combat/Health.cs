ab44521e8e2705b7adaa2910c22ebb26d585b5da
Christopher Nye <icedcrow@yahoo.com>
2022-05-30 21:41:59 -0700

    Dealing Damage

---
+++ Assets/Code/Combat/Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Unity3rdPersonDemo.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] private int maxHealth;
+        private int _health;
+
+        /// <summary>
+        /// The current health of the object.
+        /// </summary>
+        public int CurrentHealth => _health;
+
+        private void Start()
+        {
+            _health = maxHealth;
+        }
+
+        /// <summary>
+        /// Damages the component with the amount given.
+        /// </summary>
+        /// <param name="damage">The amount of damage to apply.</param>
+        public void DamageHealth(int damage)
+        {
+            if (_health == 0) return;
+            _health -= damage;
+            if (_health < 0) _health = 0;
+
+            Debug.Log($"Target is damaged and has {_health} remaining");
+        }
+    }
+}
--- Assets/Code/Combat/Weapon.cs
+++ Assets/Code/Combat/Weapon.cs
@@ -1,19 +1,59 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Unity3rdPersonDemo.Combat
 {
+    /// <summary>
+    /// Represents the currently equipped weapon of the character.
+    /// </summary>
     public class Weapon : MonoBehaviour
     {
-        [SerializeField] [Tooltip("The collider of the weapon")] private GameObject weapon;
+        [SerializeField] [Tooltip("The collider of the weapon")] private GameObject weaponCollider;
+        [SerializeField] private Collider owningCharacter;
+        [SerializeField] private int baseDamage;
+        
+        private readonly List<Collider> _itemsAlreadyHit = new();
+
+        private void Start()
+        {
+            weaponCollider.gameObject.TryGetComponent(out WeaponAction swing);
+            swing.OnWeaponSwinging += OnWeaponSwinging;
+        }
+
+        private void OnDestroy()
+        {
+            weaponCollider.gameObject.TryGetComponent(out WeaponAction swing);
+            swing.OnWeaponSwinging -= OnWeaponSwinging;
+        }
+
+        private void OnTriggerEnter(Collider target)
+        {
+            if (target == owningCharacter) return; //dont let me hit myself
+
+            if (_itemsAlreadyHit.Contains(target)) return; //dont hit the same thing twice in one swing
+
+            //This should trigger when the collider on the weapon collides with a rigid body
+            target.gameObject.TryGetComponent(out Health health);
+            if (health == null) return; //not something you can damage
+
+            health.DamageHealth(baseDamage);
+            _itemsAlreadyHit.Add(target);
+        }
 
         public void EnableWeapon()
         {
-            weapon.SetActive(true);
+            weaponCollider.SetActive(true);
         }
 
         public void DisableWeapon()
         {
-            weapon.SetActive(false);
+            weaponCollider.SetActive(false);
+        }
+
+        private void OnWeaponSwinging()
+        {
+            //every time the weapon begins to swing, clear the items already hit list.
+            _itemsAlreadyHit.Clear();
         }
     }
 }
